43911ce417c977daaee386461f2fa81b5ec850b1
danny.197 <hyg4work@gmail.com>
2026-02-12 14:18:29 +0900

    refactor : matchCount 를 list 로 수정

--- src/main/java/lotto/model/WinningRank.java
+++ src/main/java/lotto/model/WinningRank.java
@@ -7,27 +7,27 @@ import java.util.stream.Collectors;
 
 public enum WinningRank {
 
-    NONE(0, 1000, 0, 0),
-    FIFTH(5000, 5, 3, 0),
-    FOURTH(50000, 4, 4, 0),
-    THIRD(1_500_000, 3, 5, 0),
-    SECOND(30_000_000, 2, 5, 1),
-    FIRST(2_000_000_000, 1, 6, 0);
+    NONE(0, 1000, 0, 0, 1, 2),
+    FIFTH(5000, 5, 0, 3),
+    FOURTH(50000, 4, 0, 4),
+    THIRD(1_500_000, 3, 0, 5),
+    SECOND(30_000_000, 2, 1, 5),
+    FIRST(2_000_000_000, 1, 0, 6);
 
     public final Money winningPrice;
     public final int rank;
-    private final int matchCount;
+    private final List<Integer> matchCounts;
     private final int bounceCount;
 
-    WinningRank(int winningPrice, int rank, int matchCount, int bounceCount) {
+    WinningRank(int winningPrice, int rank, int bounceCount, Integer... matchCounts) {
         this.winningPrice = new Money(winningPrice);
         this.rank = rank;
-        this.matchCount = matchCount;
         this.bounceCount = bounceCount;
+        this.matchCounts = Arrays.asList(matchCounts);
     }
 
-    private boolean isSatisfied(int matchCount, int bounceCount) {
-        return matchCount >= this.matchCount
+    public boolean isSatisfied(int matchCount, int bounceCount) {
+        return matchCounts.contains(matchCount)
                 && bounceCount >= this.bounceCount;
     }
 
@@ -39,7 +39,7 @@ public enum WinningRank {
     }
 
     public int getMatchCount() {
-        return matchCount;
+        return matchCounts.getFirst();
     }
 
     public int getBounceCount() {
@@ -48,7 +48,7 @@ public enum WinningRank {
 
     public static List<WinningRank> getValidRanks() {
         return Arrays.stream(values())
-                .filter(rank -> rank.matchCount > 0)
+                .filter(rank -> !rank.equals(NONE))
                 .sorted(Comparator.comparingInt(r -> -r.rank))
                 .collect(Collectors.toList());
     }
